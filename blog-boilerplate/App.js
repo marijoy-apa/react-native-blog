fa450fdaf0e4f4cd27d2538fb753c60356972ef1
marijoyapa <marijoy.apa@tooltwist.com>
2024-02-06 16:34:09 +0800

    setup

---
+++ blog-boilerplate/App.js
@@ -0,0 +1,20 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import React from 'react';
+import IndexScreen from './src/screens/IndexScreen';
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  })
+
+const App = createAppContainer(navigator);
+export default () => {
+  return <App />
+}
